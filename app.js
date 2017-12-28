2242087ed4e2cbd5da3af5bb8ff11c475649ef1c
Oizopower <oizopower@whitecoin.info>
2017-12-28 23:07:08 +0100

    Add network staking info as networkhash on overview page

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ internation.set(app);
 bitcoinapi.setWalletDetails(settings.wallet);
 if (settings.heavy != true) {
   bitcoinapi.setAccess('only', ['getinfo', 'getnetworkhashps', 'getmininginfo','getdifficulty', 'getconnectioncount',
-    'getblockcount', 'getblockhash', 'getblock', 'getrawtransaction', 'getpeerinfo', 'gettxoutsetinfo']);
+    'getblockcount', 'getblockhash', 'getblock', 'getrawtransaction', 'getpeerinfo', 'gettxoutsetinfo','getstakinginfo']);
 } else {
   // enable additional heavy api calls
   /*
@@ -47,7 +47,7 @@ if (settings.heavy != true) {
   bitcoinapi.setAccess('only', ['getinfo', 'getstakinginfo', 'getnetworkhashps', 'getdifficulty', 'getconnectioncount',
     'getblockcount', 'getblockhash', 'getblock', 'getrawtransaction','getmaxmoney', 'getvote',
     'getmaxvote', 'getphase', 'getreward', 'getnextrewardestimate', 'getnextrewardwhenstr',
-    'getnextrewardwhensec', 'getsupply', 'gettxoutsetinfo']);
+    'getnextrewardwhensec', 'getsupply', 'gettxoutsetinfo', 'getstakinginfo']);
 }
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
